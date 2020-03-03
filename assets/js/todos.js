5f5f7c7d8739b1efeed869ac7cb9e32a585a585b
Kamyar <54872856+noakishere@users.noreply.github.com>
2020-03-03 16:21:05 -0500

    Add files via upload

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,13 +1,25 @@
 // Check Off Specific Todos By Clicking
-$("li").click(function(){
+$("ul").on("click","li", function(){
 	//if li is gray turn it black
 	$(this).toggleClass("completed");
 });
 
 //Click on X to delete Todo
-$("span").click(function(){
+$("ul").on("click","span",function(){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+
+		//create a new li and add to ul
+		$("ul").append("<li>"+"<span>X</span> "+todoText+"</li>");
+
+	}
 });
